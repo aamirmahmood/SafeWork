b49313c6ed98ab898665633214b2a3aad5aefcc1
aamirmahmood <aamir.mahmud@gmail.com>
2025-09-18 16:35:35 +0200

    Add files via upload

---
+++ CPP Localization/3dR_SRS/ranging3D.m
@@ -0,0 +1,219 @@
+%Draft of a PRS-based scenario of CP in OFDM
+clear all; clc; %#ok<CLALL>
+
+%% Prepare Excel sheet
+xlsFilename = 'simResults.xlsx';
+
+% Check if file exists
+if isfile(xlsFilename)
+    % If file exists, count the number of existing sheets
+    [~, xlsSheets] = xlsfinfo(xlsFilename);
+    xlsSheetNumber = numel(xlsSheets) + 1; % New sheet numbered sequentially
+else
+    % If the file does not exist, we start from sheet 1
+    xlsSheetNumber = 1;
+end
+% Sheet name
+xlsSheetName = ['Sim_' num2str(xlsSheetNumber)];
+
+% Adding headers
+xlsHeaders = {'Reference', 'Estimated', 'Semi Results'};
+writecell(xlsHeaders, xlsFilename, 'Sheet', xlsSheetName, 'Range', 'A1:J1');
+
+% We initialize the row variable to track the row number after headers
+xlsRow = 2;
+
+%% Use "qd_builder.supported_scenarios" in command line to get all possible scenarios
+% Other possible scenarios 
+% 3GPP_38.901_InF_LOS
+% 3GPP_38.901_InF_NLOS_SL
+% 3GPP_38.901_InF_NLOS_SH
+% 3GPP_38.901_InF_NLOS_DL
+% 3GPP_38.901_InF_NLOS_DH
+
+scenario = '3GPP_38.901_InF_LOS';   % Channel model
+
+%%%%%%%%%%%% tx BSs positions %%%%%%%%%%%%
+tx_position( : ,1) = [20; 120; 15];       % Position of BS-1 at (x; y; height) in meters
+tx_position( : ,2) = [120; 20; 10];       % Position of BS-2 at (x; y; height) in meters
+tx_position( : ,3) = [120; 120; 18];       % Position of BS-3 at (x; y; height) in meters
+
+rx_mov = [2.5; 5; 1];     %RX movement in (x; y; speed)
+
+for pos = 0:5
+
+    rx_position = [40 + (rx_mov(1) *pos); 60 + (rx_mov(2) * pos); 1];       % Position of the receiver at (x; y; height) in meters
+
+    % Dump reference position to the file
+    writematrix(rx_position, xlsFilename, 'Sheet', xlsSheetName, 'Range', ['A' num2str(xlsRow)]);
+
+    %% Generate a SRS block
+    subcarrierSpacing = 30e3;
+    numSrsSymbols = 8;  
+    symbolStartIdx = 0;
+    cSrs = 63;
+    combNumber = 4; 
+    
+    [SRS_freq_tx, ~] = SRSSequence_5g(numSrsSymbols, symbolStartIdx, cSrs, combNumber);
+    % SRS should be a 14-column vector of frequency coefficients. 273 RBs x 12 SC is 3276 subcarriers and for 30kHz SCS gives 100 MHz BW for each symbol.
+    
+    for symbolIdx = symbolStartIdx + 1 : symbolStartIdx + numSrsSymbols
+    
+        %% Get channel impulse response and create signal received at UE.
+        [channel_freq_response, ~] = CIRs(tx_position, rx_position, scenario, rx_mov(3));                  % Channel impulse response modeled in QuaDriGa of size 3276 x N, where is N no. of gNbs's
+        SRS_freq_rx = channel_freq_response .* SRS_freq_tx(:, symbolIdx);
+    
+        %% Checkpoint to show PRS at Tx and Rx
+    %     figure(1)
+    %         subplot(3,2,1); plot(abs(SRS_freq_tx(:, symbolIdx))); xlabel('subcarrier index [n]'); ylabel('amplitude SRS_{tx}');
+    %         subplot(3,2,2); plot(angle(SRS_freq_tx(:, symbolIdx))); xlabel('subcarrier index [n]'); ylabel('angle SRS_{tx}');
+    %         subplot(3,2,3); plot(abs(SRS_freq_rx)); xlabel('subcarrier index [n]'); ylabel('amplitude SRS_{rx}');
+    %         subplot(3,2,4); plot(angle(SRS_freq_rx)); xlabel('subcarrier index [n]'); ylabel('angle SRS_{rx}');
+    
+         %% Prepare freq. block for processing 
+         num_subcarriers = 3276;        % Actual no. of subcarriers
+         speed_of_light = 299792458;    % Speed of light 
+    
+         % Excluding 0 freq. at Tx 
+         % SRS_freq_nz_tx = []; 
+         i=1;
+         for n=1:num_subcarriers
+            if SRS_freq_tx(n,symbolIdx) ~= 0
+                SRS_freq_nz_tx(i) = SRS_freq_tx(n,symbolIdx); %#ok<*SAGROW> 
+                i = i + 1;
+            end
+         end
+    
+     for j=1 : size(tx_position, 2)
+    
+         % Excluding 0 freq at Rx
+         % SRS_freq_nz_rx = [];
+         i=1;
+         for n=1 : num_subcarriers
+            if (SRS_freq_rx(n,j) ~= 0)
+                SRS_freq_nz_rx(j,i) = SRS_freq_rx(n,j); 
+                i = i + 1;
+            end
+         end
+    
+        % Phase shift by the initial offset.
+        SRS_freq_nz_rx(j,:) = angle(SRS_freq_nz_rx(j,:)) - angle(SRS_freq_nz_tx);
+        
+        %% Differential phase measurements 
+        % SRS_delta_phi_1 = [];
+        % Multicase to keep the phase continuous 
+        i=1;
+        for n=2:size(SRS_freq_nz_rx, 2)
+            if ((SRS_freq_nz_rx(j, n) - SRS_freq_nz_rx(j, n-1)) > pi)
+                SRS_delta_phi_1(j, i) = abs((2*pi) - (SRS_freq_nz_rx(j, n) - SRS_freq_nz_rx(j, n-1)));
+    
+            elseif ((SRS_freq_nz_rx(j, n) - SRS_freq_nz_rx(j, n-1)) < -pi)
+                SRS_delta_phi_1(j, i) = abs((2*pi) + (SRS_freq_nz_rx(j, n) - SRS_freq_nz_rx(j, n-1)));
+    
+            elseif ((SRS_freq_nz_rx(j, n) - SRS_freq_nz_rx(j, n-1)) < 0)
+               SRS_delta_phi_1(j, i) = abs(-(SRS_freq_nz_rx(j, n) - SRS_freq_nz_rx(j, n-1)));
+    
+            else
+                SRS_delta_phi_1(j, i) = abs((SRS_freq_nz_rx(j, n) - SRS_freq_nz_rx(j, n-1)));
+            end
+            i = i + 1;
+        end
+    
+        delta_phi_1(j,:) = mean(SRS_delta_phi_1(j,:)./(2*pi));
+        distance_1(j, symbolIdx) = delta_phi_1(j)/(1/(speed_of_light/(combNumber*subcarrierSpacing))); %/720000 -> 0.0024;
+    
+        %% Second Approach
+        % SRS_delta_phi_m2 = [];
+        i=1;
+        for n=1:size(SRS_freq_nz_rx, 2)/2
+            if ((SRS_freq_nz_rx(j, ((size(SRS_freq_nz_rx, 2)/2) + n)) - SRS_freq_nz_rx(j,n)) > pi)
+                SRS_delta_phi_m2(j,i) = abs((2*pi) - (SRS_freq_nz_rx(j, (size(SRS_freq_nz_rx, 2)/2) + n) - SRS_freq_nz_rx(j,n)));
+    
+            elseif ((SRS_freq_nz_rx(j, ((size(SRS_freq_nz_rx, 2)/2) + n)) - SRS_freq_nz_rx(j,n)) < -pi)
+                SRS_delta_phi_m2(j,i) = abs((2*pi) + (SRS_freq_nz_rx(j, (size(SRS_freq_nz_rx, 2)/2) + n) - SRS_freq_nz_rx(j,n)));
+    
+            elseif ((SRS_freq_nz_rx(j, ((size(SRS_freq_nz_rx, 2)/2) + n)) - SRS_freq_nz_rx(j,n)) < 0)
+               SRS_delta_phi_m2(j,i) = abs(-((SRS_freq_nz_rx(j, (size(SRS_freq_nz_rx, 2)/2) + n)) - SRS_freq_nz_rx(j,n)));
+    
+            else
+                SRS_delta_phi_m2(j,i) = abs((SRS_freq_nz_rx(j, (size(SRS_freq_nz_rx, 2)/2) + n)) - SRS_freq_nz_rx(j,n));
+            end
+            i = i + 1;
+        end
+    
+        delta_phi_m2(j,:) = mean(SRS_delta_phi_m2(j,:)./(2*pi));
+        del_N_m2(j) = floor((size(SRS_freq_nz_rx, 2)/2) * delta_phi_1(j) - delta_phi_m2(j));
+        
+        distance_m2(j, symbolIdx) = (delta_phi_m2(j) + del_N_m2(j))/(1/(speed_of_light/(num_subcarriers/2 * subcarrierSpacing))); %/196560000 -> 0.6552;
+    
+        %% Checkpoint to show results 
+    %     figure(1) 
+    %         subplot(3,2,5); plot(SRS_delta_phi_1(j,:)); xlabel('subcarrier index [n]'); ylabel('angle \Delta\phi_{1} [rad]');
+    %         subplot(3,2,6); plot(SRS_delta_phi_m2(j,:)); xlabel('subcarrier index [n]'); ylabel('angle \Delta\phi_{m/2} [rad]');
+    %     pause
+    
+     end
+    end
+    
+    m_distance_1 = min(distance_1, [], 2);
+
+    %% Trilateration
+    p = tx_position;
+    r = m_distance_1';
+    [sol1, sol2] = trilateration(p, r);
+    
+    % Solutions
+    fprintf('\n Solutions: \n');
+    disp([sol1(2:4,1) sol2(2:4,1)])
+
+    % Save estimated position to the file
+    writematrix(sol2(2:4,1), xlsFilename, 'Sheet', xlsSheetName, 'Range', ['B' num2str(xlsRow)]);
+    
+        %% Checkpoint to show results 
+    % Translate sphere to new location and plot as a surface.
+%    figure(1)
+%       [x1,y1,z1] = sphere;
+%       [x2,y2,z2] = sphere;
+%       [x3,y3,z3] = sphere;
+%       % Scale to desire radius.
+%       radius1 = m_distance_1(1,1);
+%       radius2 = m_distance_1(2,1);
+%       radius3 = m_distance_1(3,1);
+%     
+%       x1 = x1 * radius1;
+%       y1 = y1 * radius1;
+%       z1 = z1 * radius1;
+%     
+%       x2 = x2 * radius2;
+%       y2 = y2 * radius2;
+%       z2 = z2 * radius2;
+%     
+%       x3 = x3 * radius3;
+%       y3 = y3 * radius3;
+%       z3 = z3 * radius3;
+%     
+%     
+%       surf(x1+tx_position(1,1), y1+tx_position(2,1), z1+tx_position(3,1), 'FaceAlpha',0.5); hold on
+%       surf(x2+tx_position(1,2), y2+tx_position(2,2), z2+tx_position(3,2), 'FaceAlpha',0.5); hold on
+%       surf(x3+tx_position(1,3), y3+tx_position(2,3), z3+tx_position(3,3), 'FaceAlpha',0.5);
+%       % Label axes.
+%       xlabel('X', 'FontSize', 5);
+%       ylabel('Y', 'FontSize', 5);
+%       zlabel('Z', 'FontSize', 5);
+%       axis equal
+%    pause
+    
+    %% Additional data
+    semiResults = [];
+    for c = 1:size(distance_1,2)
+        rInt = distance_1(:,c)';
+        [~, solInt] = trilateration(p, rInt);
+        semiResults = [semiResults solInt(2:4,1)];
+    end
+
+    % Drop also semi-results to the file
+    writematrix(semiResults, xlsFilename, 'Sheet', xlsSheetName, 'Range', ['C' num2str(xlsRow)]);
+
+    %% Update row number
+    xlsRow = xlsRow + 4;
+end
